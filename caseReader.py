d050e5fa199e8b871baf4427d001002c1f3b4750
Peter Rohrer <peter@Peters-MacBook-Pro.local>
2020-03-19 21:51:19 -0400

    Updated caseReader for Weights DOE tests

--- caseReader.py
+++ caseReader.py
@@ -9,30 +9,30 @@ cr = om.CaseReader("cases.sql")
 # get a list of cases that were recorded by the driver
 driver_cases = cr.list_cases('driver')
 
-# # report number of cases
-# print(len(driver_cases))
-#
-# # get the first driver case and inspect the variables of interest
-# case = cr.get_case(driver_cases[0])
-#
-# objectives = case.get_objectives()
-# design_vars = case.get_design_vars()
-# constraints = case.get_constraints()
-#
-# print(objectives['resist.R'])
-# print(design_vars)
-# print(constraints)
-#
-# # get a list of cases that we manually recorded
-# print(cr.list_cases('problem'))
+# report number of cases
+print(len(driver_cases))
 
-# get the final case and inspect the variables of interest
-case = cr.get_case('final')
+# get the first driver case and inspect the variables of interest
+case = cr.get_case(driver_cases[0])
 
 objectives = case.get_objectives()
 design_vars = case.get_design_vars()
 constraints = case.get_constraints()
 
-print(objectives['resist.R'])
+print(objectives['wts.Wt'])
 print(design_vars)
 print(constraints)
+
+# get a list of cases that we manually recorded
+print(cr.list_cases('problem'))
+
+# get the final case and inspect the variables of interest
+# case = cr.get_case('final')
+#
+# objectives = case.get_objectives()
+# design_vars = case.get_design_vars()
+# constraints = case.get_constraints()
+#
+# print(objectives['wts.Wt'])
+# print(design_vars)
+# print(constraints)
